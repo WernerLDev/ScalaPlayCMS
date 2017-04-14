0c37fc170dda7efb55cfdd99ace8d8cfcc3f62e0
WernerLDev <werner@icedev.nl>
2017-04-14 23:06:41 +0200

    Cleaned up maincontroller

--- app/assets/core/javascripts/api/api.js
+++ app/assets/core/javascripts/api/api.js
@@ -36,15 +36,15 @@ export function getPageTypes() {
 }
 
 export function getDocuments() {
-    return ApiCall("/documents", "GET");
+    return ApiCall("/api/v1/documents", "GET");
 }
 
 export function getDocument(id) {
-    return ApiCall("/documents/" + id, "GET");
+    return ApiCall("/api/v1/documents/" + id, "GET");
 }
 
 export function deleteDocument(id) {
-    return ApiCall("/documents/" + id, "DELETE");
+    return ApiCall("/api/v1/documents/" + id, "DELETE");
 }
 
 export function addDocument(parent_id, name, pagetype) {
@@ -53,49 +53,49 @@ export function addDocument(parent_id, name, pagetype) {
         "name" : name,
         "pagetype": pagetype
     })
-    return ApiCall("/documents", "POST", body);
+    return ApiCall("/api/V1/documents", "POST", body);
 }
 
 export function updateDocument(document) {
     var body = JSON.stringify({
         "document" : document
     });
-    return ApiCall("/documents", "PUT", body);
+    return ApiCall("/api/v1/documents", "PUT", body);
 }
 
 export function collapseDocument(id, collapsed) {
     var body = JSON.stringify({
         "collapsed": collapsed
     });
-    return ApiCall("/documents/" + id + "/collapse", "PUT", body);
+    return ApiCall("/api/v1/documents/" + id + "/collapse", "PUT", body);
 }
 
 export function renameDocument(id, name) {
     var body = JSON.stringify({
         "name": name
     });
-    return ApiCall("/documents/" + id + "/rename", "PUT", body);
+    return ApiCall("/api/v1/documents/" + id + "/rename", "PUT", body);
 }
 
 export function updateParentDocument(id, parent_id) {
     var body = JSON.stringify({
         "parent_id": parent_id
     });
-    return ApiCall("/documents/" + id + "/updateparent", "PUT", body);
+    return ApiCall("/api/v1/documents/" + id + "/updateparent", "PUT", body);
 }
 
 export function updateDocumentPublishDate(id, publishdate) {
     var body = JSON.stringify({
         "publishdate" : publishdate
     });
-    return ApiCall("/documents/"+id+"/publishdate", "PUT", body);
+    return ApiCall("/api/v1/documents/"+id+"/publishdate", "PUT", body);
 }
 
 export function SaveEditables(id, editables) {
     var body = JSON.stringify({
         "editables": editables
     });
-    return ApiCall("/documents/" + id + "/editables", "PUT", body);
+    return ApiCall("/api/v1/documents/" + id + "/editables", "PUT", body);
 }
 
 export function getAssets() {
--- app/core/controllers/MainController.scala
+++ app/core/controllers/MainController.scala
@@ -34,116 +34,6 @@ class MainController @Inject()(documents:Documents, editables:Editables, templat
     Ok(core.views.html.index("Logged in as " + request.user.username, token.value))
   }
 
-  def getPageTypes = WithAuthAction {
-    val pagetypes = templates.templates.map { case (k,v) => {
-      Json.toJson(Map( "typekey" -> JsString(k), "typename" -> JsString(v.name)))
-     }}.toSeq
-    Ok(
-      Json.toJson(
-        Map(
-          "pagetypes" -> JsArray(pagetypes)
-        )
-      )
-    )
-  }
-
-  implicit val DocWrites = Json.writes[DocumentJson]
-  def listDocuments = WithAuthAction.async {
-    documents.listJson map (x => {
-      //Thread.sleep(500);
-      Ok(Json.toJson(x))
-    })
-  }
-
-  implicit val DocumentWrites = Json.writes[Document]
-  def getDocument(id:Long) = WithAuthAction.async {
-    documents.getById(id).map(docOpt => {
-      docOpt.map(doc => Ok(Json.toJson(doc)))
-      .getOrElse(BadRequest("Document not found"))
-    })
-  }
-
-  def deleteDocument(docid:Long) = WithAuthAction.async {
-    documents.delete(docid) map ((affectedRows:Int) => {
-      Ok(Json.toJson( Map("success" -> JsBoolean(true), "affectedRows" -> JsNumber(affectedRows)) ))
-    })
-  }
-
-  def addDocument() = WithAuthAction.async(parse.json) { request =>
-      val parent_id = (request.body \ "parent_id").asOpt[Int].getOrElse(0)
-      val inputname = (request.body \ "name").asOpt[String].getOrElse("")
-      val name = inputname.toLowerCase.replace(" ", "-")
-      val pagetype = (request.body \ "pagetype").asOpt[String].getOrElse("default")
-      val currentTime:Timestamp = new Timestamp((new Date).getTime());
-      val parent = documents.getById(parent_id)
-      parent flatMap (docOp => docOp match {
-        case Some(parentDoc) => {
-          //val newpath = if(parentDoc.parent_id > 0) parentDoc.path + "/" + name else "/" + name
-          val newpath = if(parentDoc.doctype == "home") "/" + name else parentDoc.path + "/" + name
-          val newDocument = Document(
-            id = 0, parent_id = 0,  name = name,
-            doctype = "page", path = newpath, collapsed = true,
-            view = Some(pagetype), title = "", locale = "en", description = "",
-            created_at = currentTime, updated_at = currentTime, published_at = currentTime
-          )
-          documents.create( newDocument ) map { x =>
-            Ok(Json.toJson( Map("id" -> JsNumber(x.id),  "parent_id" -> JsNumber(parent_id), "name" -> JsString(name)) ))
-          }
-        }
-        case None => Future(BadRequest("Invalid parent_id"))
-      })
-  }
-  
-  implicit val DocumentReads = Json.reads[Document]
-  def updateDocument = WithAuthAction.async(parse.json) { request => 
-    {request.body \ "document"}.asOpt[Document].map( document => {
-      documents.update(document) map (x => {
-        Ok( Json.toJson(Map("success" -> JsBoolean(true))) )
-      })
-    }).getOrElse(Future(BadRequest("Parameter missing")))
-  }
-
-  def collapseDocument(id:Long) = WithAuthAction.async(parse.json) { request =>
-    val collapseState = (request.body \ "collapsed").asOpt[Boolean]
-    (collapseState map { collapse:Boolean =>
-      documents.setCollapsed(id, collapse) map { x =>
-        Ok(Json.toJson(Map("success" -> JsNumber(x))))
-      }
-    }).getOrElse( Future(BadRequest("Error: Missing parameter [collapsed]")) )
-  }
-
-  def renameDocument(id:Long) = WithAuthAction.async(parse.json) { request =>
-    ((request.body \ "name").asOpt[String].map{ name =>
-      documents.setName(id, name) map { x =>
-        Ok(Json.toJson(Map("success" -> JsNumber(x))))
-      }
-    }).getOrElse( Future(BadRequest("Error: missing parameter [name]")) )
-  }
-
-  def updateParentDocument(id:Long) = WithAuthAction.async(parse.json) { request =>
-    ((request.body \ "parent_id").asOpt[Long].map{ parent_id =>
-      documents.updateParent(id, parent_id) map { x =>
-        Ok(Json.toJson(Map("success" -> JsNumber(x))))
-      }
-    }).getOrElse( Future(BadRequest("Error: missing parameter [parent_id]")) )
-  }
-
-  def setDocumentPublishDate(id:Long) = WithAuthAction.async(parse.json) { request => 
-    ((request.body \ "publishdate").asOpt[Long].map{ publishdate =>
-      documents.updatePublishDate(id, publishdate) map { x =>
-        Ok(Json.toJson(Map("success" -> JsNumber(x))))
-      }
-    }).getOrElse( Future(BadRequest("Error: missing parameter [publishdate]")) )
-  }
-
-  implicit val EditableReads = Json.reads[Editable]
-  def saveEditables(id:Long) = WithAuthAction(parse.json) { request => 
-    {request.body \ "editables"}.asOpt[List[Editable]].map( elist => {
-        elist foreach (e => editables.insertOrUpdate(e))
-        Ok( Json.toJson(Map("success" -> JsBoolean(true))) )
-    }).getOrElse(BadRequest("Parameter missing"))
-  }
-
   def page(path:String) = Action.async { implicit request =>
     documents.getByPath("/" + path) flatMap (docOpt => docOpt match {
       case Some(p) => templates.getAction(p)(request)
@@ -155,6 +45,5 @@ class MainController @Inject()(documents:Documents, editables:Editables, templat
       }
     })
   }
-
 
 }
